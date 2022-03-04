23a584984330cb50b4ac57078b4356d3845bd76d
roquesrolando <roquesrolando@hotmail.com>
2022-03-04 14:53:23 -0400

    added calculations for score

--- extractRarityScore.js
+++ extractRarityScore.js
@@ -6,6 +6,7 @@ const fspath = require("fspath");
 
 const metadata = require("./client/config/allMetadata.json");
 const percentageDistribution = require("./client/config/percentageDistribution.json");
+const stats = ['Health', 'Attack', 'Defense', 'Heal', 'Soul']
 
 let traits = [];
 for (let m of metadata) {
@@ -19,20 +20,36 @@ let per = [];
 for (let i = 0; i < traits.length; i++) {
   //j attribute per user
 
-  let totalScore = 0;
+  let totalStats = 0;
+  let totalAttributes = 0;
+  let totalRarity = 0;
   for (let j = 0; j < traits[i][1].length; j++) {
     for (let p in percentageDistribution) {
       if (p === traits[i][1][j].trait_type) {
         const key = Object.keys(percentageDistribution[p]);
         key.forEach((val, index) => {
           if (val === traits[i][1][j].value) {
-            totalScore =
-              totalScore + 1 / (percentageDistribution[p][val] / 100);
+            if (stats.includes(traits[i][1][j].trait_type)) {
+            totalStats =
+              totalStats + 1 / (percentageDistribution[p][val] / 100);
+            }
+            else if (traits[i][1][j].trait_type === "Rarity") {
+              totalRarity =
+              totalRarity + 1 / (percentageDistribution[p][val] / 100);
+              totalRarity = totalRarity * 100
+            }
+            else {
+              totalAttributes =
+              totalAttributes + 1 / (percentageDistribution[p][val] / 100);
+              totalAttributes = totalAttributes * 3
+            }
           }
         });
       }
     }
   }
+  totalScore = totalAttributes + totalStats + totalRarity
+  console.log(totalScore)
   per.push([traits[i][0], totalScore]);
 }
 
