1655dd118b566eb822ce66cc1e5be83ea6b3a2d2
jerrybassat7 <jerry.bassat@upr.edu>
2022-03-04 15:43:50 -0400

    tweaking the values to give more importance to certain atrributes

---
+++ bin/run-scripts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+node extractPercentageDistribuition.js 
+node extractRarityDistribution.js 
+node extractRarityScore.js 
+node addRarityScoreToAll.js
+node sortByRarityScore.js  
--- extractRarityScore.js
+++ extractRarityScore.js
@@ -7,6 +7,7 @@ const fspath = require("fspath");
 const metadata = require("./client/config/allMetadata.json");
 const percentageDistribution = require("./client/config/percentageDistribution.json");
 const stats = ["Health", "Attack", "Defense", "Heal", "Soul"];
+const abilities = ["Active Ability 1" , "Active Ability 2", "Passive Ability", "Leader Ability"]
 
 let traits = [];
 for (let m of metadata) {
@@ -23,6 +24,8 @@ for (let i = 0; i < traits.length; i++) {
   let totalStats = 0;
   let totalAttributes = 0;
   let totalRarity = 0;
+  let totalAbilities = 0;
+  let totalTier = 0;
   for (let j = 0; j < traits[i][1].length; j++) {
     for (let p in percentageDistribution) {
       if (p === traits[i][1][j].trait_type) {
@@ -32,21 +35,34 @@ for (let i = 0; i < traits.length; i++) {
             if (stats.includes(traits[i][1][j].trait_type)) {
               totalStats =
                 totalStats + 1 / (percentageDistribution[p][val] / 100);
+                totalStats = totalStats * .1
             } else if (traits[i][1][j].trait_type === "Rarity") {
               totalRarity =
                 totalRarity + 1 / (percentageDistribution[p][val] / 100);
-              totalRarity = totalRarity * 100;
-            } else {
+              totalRarity = totalRarity * 10;
+
+            }else if (traits[i][1][j].trait_type === "Tier") {
+              totalTier =
+                totalTier + 1 / (percentageDistribution[p][val] / 100);
+              totalTier = totalTier * 2;
+
+            } else if (abilities.includes(traits[i][1][j].trait_type)) {
+              totalAbilities =
+              totalAbilities + 1 / (percentageDistribution[p][val] / 100);
+              totalAbilities = totalAbilities * 1.1;
+
+            } 
+            else {
               totalAttributes =
                 totalAttributes + 1 / (percentageDistribution[p][val] / 100);
-              totalAttributes = totalAttributes * 3;
+              totalAttributes = totalAttributes * 1;
             }
           }
         });
       }
     }
   }
-  totalScore = totalAttributes + totalStats + totalRarity;
+  totalScore = totalAttributes + totalStats + totalRarity + totalAbilities;
   console.log(totalScore);
   per.push([traits[i][0], totalScore]);
 }
