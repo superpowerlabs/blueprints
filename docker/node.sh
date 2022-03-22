11256886b5f290d16920851defb5d83c70d4382a
Francesco Sullo <sullof@gmail.com>
2022-03-22 17:22:22 -0500

    Blue > Super

--- docker/node.sh
+++ docker/node.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-docker stop blue-micio-net
-docker rm blue-micio-net
+docker stop super-micio-net
+docker rm super-micio-net
 
 docker run -d \
-  --name blue-micio-net \
+  --name super-micio-net \
   -p 8734 \
   --restart unless-stopped \
   -v $PWD:/usr/src/app \
-  -v /vol/log/blue-micio-net_app:/var/log/blue-micio-net_app \
+  -v /vol/log/super-micio-net_app:/var/log/super-micio-net_app \
   -e NODE_ENV=production \
-  -e VIRTUAL_HOST=blue.micio.net \
-  -e LETSENCRYPT_HOST=blue.micio.net \
+  -e VIRTUAL_HOST=super.micio.net \
+  -e LETSENCRYPT_HOST=super.micio.net \
   -e LETSENCRYPT_EMAIL=something@sullo.co \
   -w /usr/src/app node:16 npm run start
