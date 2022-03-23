28ef76c535189fad5bfb87b2a5795d233ad5c232
Francesco Sullo <sullof@gmail.com>
2022-03-23 15:26:50 -0500

    Fix overview

--- client/config/text.js
+++ client/config/text.js
@@ -1,83 +1,75 @@
 const text = `
+# Mobland Genesis Blueprint Overview
 
-# Mobland Blueprint NFTs
+Genesis Blueprints represent crew members in MOBLAND. They have a variety of appearance and statistical traits that make them unique, with literally hundreds of billions of possible combinations. When you open the game with a Genesis Blueprint NFT in your wallet, you will see a full 3D version of the crew member in-game ready for your orders.
 
-### Overview
+As an up-and-coming #MafiaMetaverse gangster, the Blueprints can be used in the following ways 👇
 
-Blueprint NFTs represent crew members in the Mobland game. They have a variety of appearance, statistical and ability traits that make them unique, with literally hundreds of billions of possible combinations. When you open the game with a Genesis Blueprint NFT in your wallet, you will see a full 3D version of the crewmember in-game ready for your orders. In the game, Crew from Blueprints represent your soldiers that you, as an up-and-coming MetaMafia gangster, can use in the following ways:
+- 🔫 Attacking other player’s Crews & Buildings
+- 🛡 Assign to Defend & Guard your Buildings and Assets
+- 🔋 Assign to Enhance your Buildings and Assets
+- 🛠 Send on Jobs to gain Resources
+- 😎 Build your brand in the #MafiaMetaverse and beyond
+- 🤑 Trade or Sell on the MOBLAND Marketplace and Beyond
 
-- Attacking other player’s Crews
+## Crew Characteristics
+Many characteristics are present on the Genesis Blueprint characters. The metadata is composed of two main categories: **Appearance & Statistics**:
 
-- Attacking other player’s Buildings
+1. **Appearance**: defines how characters look in-game
+2. **Statistics**: define a character’s basic in-game capabilities and their rarity
 
-- Assign to guard your Buildings
+The upcoming reveal will showcase the Genesis Blueprints metadata which will be followed by the launch of the 3D character viewer app. The team is hard at work and in the final testing stage of the character viewer which is slated for release shortly after the reveal.
 
-- Assign to enhance your Buildings
+## Crew Appearance
+The appearance attributes of the Genesis Blueprints define a character’s in-game appearance. Each character has a unique appearance centred around Gender, Skin Tone, Chest, Hair, Head & Leg attributes. The Appearance category of the Genesis Blueprints also dictates the character’s Personality through animated reactions. Ultimately, the combination of all the Crew Appearance characteristics will determine the rarity of your Genesis Blueprint.
 
-- Send on Jobs to gain Resources
+With that said, be careful judging the crew appearances as looks can be deceiving, especially in the MOBLAND #MafiaMetaverse. Gangsters all have their own unique swagger and our Blueprints are no different.
 
-- Show off by sharing as a PFP
+Each Blueprint will be comprised of the following Appearance Attributes: 👇
 
-- Trade or Sell on the Mobland Marketplace
+##### Rarity 💎
+This rarity rating will have in-game implications as the rating will alter the statistics of the character enabling an edge in certain gameplay types. Rarity is determined by an addition of attack, defense, health and health regeneration. It’s a fast and reliable metric to assess how many of these characteristics are above the normal levels!
+##### Gender 💁‍♀️💁‍♂️
+Male or Female character gender. This appearance attribute affects the base models of characters and what they can wear.
+##### Personality 😤
+The way a character handles themselves in terms of attitude and animation. Many characters have an attitude, and this trait affects how the player walks, moves and talks. The personality trait embodies the character’s swagger.
+##### Gang 😈
+This depicts the syndicate in the MOBLAND #Mafiametaverse a character belongs to. Gangs have synergies, alliances and hatred that has been fostered for many years throughout the #MafiaMetaverse. In-game, this means that some gangs will perform better or worse against certain types of enemies.
+##### Chest 🎽🧥👘
+The chest style of the character. Chest is the location of any type of outfit above waist. This includes shirts, jackets, tattoo, and more.
+##### Hair 💇‍♀️ 🎩 🪖 🧢 💇‍♂️
+The hairstyle of the character. This both includes hairstyle, hair color, but also hats, caps and other types of hair cosmetics.
+##### Head 💋 🕶
+The head & face style of the character. This includes faces, teeth, lips, eyebrows, makeup, anything that is effectively on the face of a character.
+##### Legs 👖🩳👠👟
+The lower body style of the character. This includes trousers, dresses, but also footwear!
 
+## Crew Statistics
+This category depicts how much damage your Crew can inflict, the defence tolerance of an opponent’s attack, the combat style your crew embodies, and specific weapons compatible with your crew.
 
-### Crew Traits
+Statistics are the core of the crew characteristics which determine a character’s gameplay capabilities and limitations in combat. The statistics will play a critical role in the outcome of battles, as they encompass attributes surrounding Health, Attack, Defense, Healing, Combat Style, Class, Weapon Type & Tier.
 
-Many characteristics are present on the Mobland Genesis Blueprint Crew individual. Three main categories: Appearance, which defines how they look in-game, Statistics which define their basic in-game capabilities and their rarity, and Abilities which are special modifiers that can affect combat, buildings, and other gameplay elements. There MIGHT be secret characteristics that are as yet unrevealed that will play an important part of the Mobland game universe, so stay alert!
+It will be wise to understand statistics and form strategies around these statistics to grow your dominance in MOBLAND. May your Genesis Blueprint harness the statistics you desire and may you strategically unleash your Genesis Blueprint within the #MafiaMetaverse on your path to supremacy.
 
-### Crew Appearance
-
-- **Rarity** – A general rating of how rare the Crew NFT is
-
-- **Gender** – Male or Female character gender
-
-- **Personality** – How the character handles themselves in terms of attitude and animation
-
-- **Gang** – What faction in the Mobland universe they belong to
-
-- **Skin Tone** – Color of their skin
-
-- **Chest** – What chest style the character has
-
-- **Hair** – What hairstyle the character has
-
-- **Head** – What head & face style the character has
-
-- **Legs** – What lower body style the character has
-
-
-### Crew Statistics
-
-- **Health** – How much damage the character can take before being wounded or killed
-
-- **Attack** – How powerful the character’s base attacks are
-
-- **Defense** – How tough the character’s base defense is
-
-- **Heal** – How fast the character heals from being wounded
-
-- **Combat Style** – Defines what combat styles the characters is strong and weak against: Reckless hurts Brutal which beats down Tactical which dunks on Reckless
-
-- **Class** – Defines the role that the character is best at: Damage, Tank, or Support
-
-- **Weapon Type** – Defines the weapon the character prefers
-
-- **Tier** – Defines the number of Abilities the character has, the higher the Tier, the more Abilities
+Each Blueprint will be comprised of the following Statistics Attributes 👇
+##### Health ⛑
+How much damage the character can sustain before being wounded or killed. Your character will heal over time, of course this is only if the character’s health does not drop below zero.
+##### Attack 🔫
+How powerful the character’s base attacks are and as a result how much damage the character is capable of inflicting. This attack modifier is added to a character’s weapon damage output, and is also altered by a character’s level. A better attack usually means that overall, that character is able to inflict more damage. This of course is contingent on the attributes of the character’s opposition
+##### Defense🛡
+How tough the character’s base defense is and how well the character holds up against opposition attacks. Defense reduces the number of damages received during team-fights. A character with a high defense usually will be able to last longer in fights giving their team a chance at withstanding the enemies onslaught of attacks
+##### Heal ❤️‍🩹
+How fast the character heals from being wounded. Healing occurs outside of combat and if the character does not reach 0 health, that character will slowly heal back up to full health. The heal characteristic determines how fast the hit-points will be gained over time.
+##### Combat Style 🪖
+Defines what combat styles the character is strong and weak against. There are three combat styles: Reckless, Brutal & Tactical. Reckless hurts Brutal which beats down Tactical which dunks on Reckless.
+##### Class 🏛
+Defines the role that the character is best at. The three classes are Damage, Tank, and Support.
+##### Weapon Type 🔫
+Defines the weapon the character prefers. This is a proficiency that will effectively make some weapons unavailable to a given character. Choose your weapons wisely!
+##### Tier 🔋
+Defines the number of abilities the character has. The higher the Tier, the more Abilities the character has.
 
 
-### Crew Abilities
-
-- **Active Abilities** – The character’s special tricks or attacks that can be activated by the player during combat
-
-- **Passive Abilities** – Enhancements or buffs that work in combat for the character without needing to be activated
-
-- **Leader Abilities** – A rating of the effectiveness the character has on the others in their crew
-
-- **Macro Abilities** – Each character has a specialized modifier that can enhance their team, what happens when they guard a building, or other in-game effect
-
-- **Traits** – Defines the profession that the character prefers that gives them bonuses in various Job assignments
-
-- **Personality** – Quirks that make the character unique and that affect various gameplay situations
 `;
 
 export default text;
