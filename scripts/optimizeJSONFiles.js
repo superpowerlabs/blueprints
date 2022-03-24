4bd502c88b63c0f6003f840dd0d41b9b9eb097e7
Francesco Sullo <sullof@gmail.com>
2022-03-24 14:35:10 -0500

    Working on optimization

---
+++ scripts/optimizeJSONFiles.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const fspath = require("fspath");
+// we do not want to re-execute this by mistake
+// process.exit(0)
+
+const metas = require("../public/json/allMetadata.json");
+
+let csv = metas[0].attributes
+  .map((e) => e.trait_type)
+  .concat("mp4", "png")
+  .join(",");
+
+let values = [];
+for (let m of metas) {
+  for (let a of m.attributes) {
+    if (values.indexOf(a.value) === -1) {
+      values.push(a.value);
+    }
+  }
+}
+
+let indexedValuesArray = new fspath("./public/json/indexedValuesArray.json");
+indexedValuesArray.write(JSON.stringify(values));
+
+for (let m of metas) {
+  csv += "\n" + m.attributes.map((e) => values.indexOf(e.value)).concat([
+    m.extras.mp4_sha256.substring(0,16),
+    m.extras.png_sha256.substring(0,16)
+  ]).join(",");
+}
+
+let output = new fspath("./public/json/metadata.csv.js");
+output.write(`module.exports = \`${csv}\``);
