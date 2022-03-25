b3dd920c1d7e5cdf65051af2950e09bad3e9fca7
Francesco Sullo <sullof@gmail.com>
2022-03-25 09:35:33 -0500

    Better organization of files 2

--- scripts/indexMetadata.js
+++ scripts/indexMetadata.js
@@ -1,35 +1,7 @@
 #!/usr/bin/env node
 
 const fspath = require("fspath");
-// const { preferredOrder } = require("../client/config");
-// we do not want to re-execute this by mistake
-// process.exit(0)
-
 const metadata = require("../input/allMetadata.json");
-//
-// let traits = {};
-// for (let m of metadata) {
-//   for (let a of m.attributes) {
-//     if (!traits[a.trait_type]) {
-//       traits[a.trait_type] = {};
-//     }
-//     if (!traits[a.trait_type][a.value]) {
-//       traits[a.trait_type][a.value] = 1;
-//     } else {
-//       traits[a.trait_type][a.value]++;
-//     }
-//   }
-// }
-//
-// let dist = {};
-// for (let key of preferredOrder) {
-//   dist[key] = traits[key];
-// }
-//
-// let output = new fspath("./public/json/rarityDistribution.json");
-// output.write(JSON.stringify(dist, null, 2));
-
-// index data
 
 let index = {};
 for (let m of metadata) {
@@ -42,5 +14,5 @@ for (let m of metadata) {
   }
 }
 
-output = new fspath("./public/json/indexedMetadata.json");
+let output = new fspath("./public/json/indexedMetadata.json");
 output.write(JSON.stringify(index, null, 2));
