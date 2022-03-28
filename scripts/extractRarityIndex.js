0f8db9357fd476542fd309ba89f38324b70d5a0c
Francesco Sullo <sullof@gmail.com>
2022-03-28 15:29:28 -0500

    Adding rarity index

--- scripts/extractRarityIndex.js
+++ scripts/extractRarityIndex.js
@@ -6,28 +6,28 @@ const fspath = require("fspath");
 
 const metadata = require("../input/allMetadata.json");
 const rarityDistribution = require("../public/json/rarityDistribution.json");
-let rarityIndex = {}
-let min = 50000
-let max = 0
+let rarityIndex = {};
+let min = 50000;
+let max = 0;
 
 for (let i = 0; i < metadata.length; i++) {
-  let a = metadata[i].attributes
-  let score = 0
+  let a = metadata[i].attributes;
+  let score = 0;
   for (let t of a) {
-    score += rarityDistribution[t.trait_type][t.value]
+    score += rarityDistribution[t.trait_type][t.value];
   }
-  min = Math.min(min, score)
-  max = Math.max(max, score)
+  min = Math.min(min, score);
+  max = Math.max(max, score);
 }
 
 for (let i = 0; i < metadata.length; i++) {
-  let a = metadata[i].attributes
-  let score = 0
+  let a = metadata[i].attributes;
+  let score = 0;
   for (let t of a) {
-    score += rarityDistribution[t.trait_type][t.value]
+    score += rarityDistribution[t.trait_type][t.value];
   }
-  score = max - score + 100
-  rarityIndex[metadata[i].tokenId] = score
+  score = max - score + 100;
+  rarityIndex[metadata[i].tokenId] = score;
 }
 
 let output = new fspath("./public/json/rarityIndex.json");
