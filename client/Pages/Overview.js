93541effeb45bed0e5808366c27cc8b314cfb692
Francesco Sullo <sullof@gmail.com>
2022-03-31 08:28:27 -0500

    Adding new Overview

--- client/Pages/Overview.js
+++ client/Pages/Overview.js
@@ -1,11 +1,171 @@
 import * as Scroll from "react-scroll";
 
-import text from "../config/text";
+
 import Markdown from "react-markdown-it";
 import Base from "../components/Base";
 
 // eslint-disable-next-line no-undef
 const { Container, Row, Col } = ReactBootstrap;
+
+const text = [
+  `
+
+# Overview
+
+Genesis Blueprints represent crew members in MOBLAND. They have a variety of appearance, statistical and ability traits that make them unique, with literally hundreds of billions of possible combinations. When you open the game with a Genesis Blueprint NFT in your wallet, you will see a full 3D version of the crew member in-game ready for your orders.
+
+As an up-and-coming #MafiaMetaverse gangster, the Blueprints can be used in the following ways 👇
+
+- 🔫 Attacking other player’s Crews & Buildings
+
+- 🛡 Assign to Defend & Guard your Buildings and Assets
+
+- 🔋 Assign to Enhance your Buildings and Assets
+
+- 🛠 Send on Jobs to gain Resources
+
+- 😎 Build your brand in the #MafiaMetaverse and beyond
+
+- 🤑 Trade or Sell on the MOBLAND Marketplace and Beyond
+
+
+### Crew Characteristics
+Many characteristics are present on the Genesis Blueprint characters. The metadata is composed of three main categories: Appearance, Statistics & Abilities:
+
+`,
+  `
+
+1. **Appearance**: defines how characters look in-game
+
+2. **Statistics**: define a character’s basic in-game capabilities and their rarity
+
+3. **Abilities**: define special modifiers that can affect combat, buildings, and other gameplay elements.
+
+
+This reveal showcases the Genesis Blueprints metadata which will be followed by the launch of the 3D character viewer app. The team is hard at work and in the final testing stage of the character viewer which is slated for release shortly after the reveal.
+
+
+## Crew Appearance
+The appearance attributes of the Genesis Blueprints define a character’s in-game appearance. Each character has a unique appearance centred around Gender, Skin Tone, Chest, Hair, Head & Leg attributes. The Appearance category of the Genesis Blueprints also dictates the character’s Personality through animated reactions. Ultimately, the combination of all the Crew Appearance characteristics will determine the rarity of your Genesis Blueprint.
+
+    `,
+  `
+
+
+With that said, be careful judging the crew appearances as looks can be deceiving, especially in the MOBLAND #MafiaMetaverse. Gangsters all have their own unique swagger and our Blueprints are no different.
+
+
+Each Blueprint will be comprised of the following Appearance Attributes: 👇
+
+
+#### Power Score 💎
+This power score will have in-game implications as the score will alter the statistics of the character enabling an edge in certain gameplay types. Power score is based on the combination of your characters, attack, defense, health, heal, and tier attributes. The higher the power score, the greater the impact a Genesis Blueprint will have in-game!
+#### Gender 💁‍♀️💁‍♂️
+Male or Female character gender. This appearance attribute affects the base models of characters and what they can wear.
+
+
+#### Personality 😤
+The way a character handles themselves in terms of attitude and animation. Many characters have an attitude, and this trait affects how the player walks, moves and talks. The personality trait embodies the character’s swagger.
+
+
+#### Gang 😈
+This depicts the syndicate in the MOBLAND #Mafiametaverse a character belongs to. Gangs have synergies, alliances and hatred that has been fostered for many years throughout the #MafiaMetaverse. In-game, this means that some gangs will perform better or worse against certain types of enemies.
+
+
+#### Chest 🎽🧥👘
+The chest style of the character. Chest is the location of any type of outfit above waist. This includes shirts, jackets, tattoo, and more.
+
+
+#### Hair 💇‍♀️ 🎩 🪖 🧢 💇‍♂️
+The hairstyle of the character. This both includes hairstyle, hair color, but also hats, caps and other types of hair cosmetics.
+
+
+#### Head 💋 🕶
+The head & face style of the character. This includes faces, teeth, lips, eyebrows, makeup, anything that is effectively on the face of a character.
+
+
+#### Legs 👖🩳👠👟
+The lower body style of the character. This includes trousers, dresses, but also footwear!
+
+
+## Crew Statistics
+This category depicts how much damage your Crew can inflict, the defence tolerance of an opponent’s attack, the combat style your crew embodies, and specific weapons compatible with your crew.
+
+
+Statistics are the core of the crew characteristics which determine a character’s gameplay capabilities and limitations in combat. The statistics will play a critical role in the outcome of battles, as they encompass attributes surrounding Health, Attack, Defense, Healing, Combat Style, Class, Weapon Type & Tier.
+
+
+`,
+  `
+It will be wise to understand statistics and form strategies around these statistics to grow your dominance in MOBLAND. May your Genesis Blueprint harness the statistics you desire and may you strategically unleash your Genesis Blueprint within the #MafiaMetaverse on your path to supremacy.
+
+
+Each Blueprint will be comprised of the following Statistics Attributes 👇
+
+
+#### Health ⛑
+How much damage the character can sustain before being wounded or killed. Your character will heal over time, of course this is only if the character’s health does not drop below zero.
+
+
+#### Attack 🔫
+How powerful the character’s base attacks are and as a result how much damage the character is capable of inflicting. This attack modifier is added to a character’s weapon damage output, and is also altered by a character’s level. A better attack usually means that overall, that character is able to inflict more damage. This of course is contingent on the attributes of the character’s opposition
+
+
+#### Defense🛡
+How tough the character’s base defense is and how well the character holds up against opposition attacks. Defense reduces the number of damages received during team-fights. A character with a high defense usually will be able to last longer in fights giving their team a chance at withstanding the enemies onslaught of attacks
+
+
+#### Heal ❤️‍🩹
+How fast the character heals from being wounded. Healing occurs outside of combat and if the character does not reach 0 health, that character will slowly heal back up to full health. The heal characteristic determines how fast the hit-points will be gained over time.
+
+
+#### Combat Style 🪖
+Defines what combat styles the character is strong and weak against. There are three combat styles: Bruiser, Defensive & Tactical. Bruiser hurts Defensive which beats down Tactical which dunks on Bruiser.
+
+
+#### Class 🏛
+Defines the role that the character is best at. The three classes are Damage, Tank, and Support.
+
+
+#### Weapon Type 🔫
+Defines the weapon the character prefers. This is a proficiency that will effectively make some weapons unavailable to a given character. Choose your weapons wisely!
+
+
+#### Tier 🔋
+Defines the number of abilities the character has. The higher the Tier, the more Abilities the character has.
+
+
+## Crew Abilities
+Abilities are a pivotal piece of the Crew characteristics and impact several gameplay elements. These attributes act as special modifiers and enhance combat styles and building capabilities. One can think of the Crew Abilities as boosters to specific attributes of your Genesis Blueprint, it is as if the respective attributes are on steroids, giving your character an advantage in relevant gameplay situations.
+
+
+
+There are two main types of Crew Abilities: Macro & Trait. The strategies implemented surrounding Crew Abilities could be the difference between a victory or a defeat. More importantly, these attributes could be the difference between a Syndicate falling out of power and another Syndicate ascending up the power rankings.
+
+
+> The question becomes: How will your Crew leverage your Genesis Blueprint Abilities to establish dominance within the #MafiaMetaverse?
+
+
+Each Blueprint will be comprised of the following Ability Attributes: 👇
+
+#### Macro Abilities 🏗
+Characters may have a specialized modifier that can enhance an in-game building. If you are more of a builder than a fighter these abilities will be of great interest to you. With a macro ability characters can be assigned to buildings as “managers”, effectively improving the outputs and effects of said buildings.
+
+
+#### Traits 🏛
+Defines the profession that the character prefers that gives them bonuses in various Job assignments
+
+
+## Genesis Blueprints be Different. Power & Rarity
+Most collections have Rarity scores to determine the value of an NFT. Rarity is the result of a calculation that focuses on the various traits, normally centered around aesthetics and attributes found in the NFTs of a collection. For Blueprints, a rarity score calculated this way would be misrepresentative of the true in-game power and value of a given Genesis Blueprint. To better represent Genesis Blueprints we have introduced a Power score.
+
+
+> Power is based on the combination of your characters, attack, defense, health, heal, and tier attributes. The higher the Power score, the greater the impact a Genesis Blueprint will have in-game.
+
+
+`,
+];
+
 
 export default class Overview extends Base {
   componentDidMount() {
@@ -28,7 +188,19 @@ export default class Overview extends Base {
                 textAlign: "left",
               }}
             >
-              <Markdown source={text} />
+              <Markdown source={text[0]} />
+              <div className={"centered padded"}>
+                <img src={"/images/black_avatar.gif"} />
+              </div>
+              <Markdown source={text[1]} />
+              <div className={"centered padded"}>
+                <img src={"/images/black_color_avatar.gif"} />
+              </div>
+              <Markdown source={text[2]} />
+              <div className={"centered padded"}>
+                <img src={"/images/color_avatar.gif"} />
+              </div>
+              <Markdown source={text[3]} />
             </div>
           </Col>
         </Row>
