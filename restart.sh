8a28e4206e052e7e964374e915c8ad8515ab4eed
Francesco Sullo <sullof@gmail.com>
2022-11-01 20:35:56 -0700

    Fix background

--- restart.sh
+++ restart.sh
@@ -1,3 +1,17 @@
 #!/bin/env bash
 
 git pull && pnpm i && pnpm build && pm2 restart blueprints
+
+#
+##!/usr/bin/env bash
+#
+#if [[ "$1" == "pull" ]]; then git pull; fi
+#
+#pnpm i
+#pnpm build
+#
+#echo "Syncing build folders"
+#rsync -a build/ build0 --delete
+#
+#pm2 restart staking
+#
